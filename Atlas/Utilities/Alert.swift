524656ecb8b0a203d689386e56b632e5cf0e7ca4
Franklin Byaruhanga <byaruhaf@appbantu.com>
2023-02-01 20:33:51 +0300

    Added Alert

---
+++ Atlas/Utilities/Alert.swift
@@ -0,0 +1,18 @@
+//
+//  Alert.swift
+//  Atlas
+//
+//  Created by Franklin Byaruhanga on 01/02/2023.
+//
+
+import Foundation
+import UIKit
+
+class Alert {
+    
+    class func showBasic(title: String, message: String, vc: UIViewController) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        vc.present(alert, animated: true)
+    }
+}
