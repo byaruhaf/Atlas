6a55fba6928b3729f6a24fb599875f22b830cfb3
Franklin Byaruhanga <byaruhaf@appbantu.com>
2023-02-03 20:36:43 +0300

    Fix SwiftLint Error's

--- Atlas/Utilities/WeatherConditionIcon.swift
+++ Atlas/Utilities/WeatherConditionIcon.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 // move to utilities and call feom here
-struct WeatherConditionIcon {
+enum WeatherConditionIcon {
     static func image(for condition: WeatherType) -> UIImage {
         switch condition {
-        case .clear: return UIImage(systemName: "sun.max.fill")!
-        case .clouds: return UIImage(systemName: "cloud.fill")!
-        case .drizzle: return UIImage(systemName: "cloud.drizzle.fill")!
-        case .rain: return UIImage(named: "rain")!
-        case .snow: return UIImage(systemName: "cloud.snow.fill")!
-        case .thunderstorm: return UIImage(systemName: "cloud.bolt.rain.fill")!
-        case .unknown: return UIImage(systemName: "cloud")!
+        case .clear:
+            return UIImage(systemName: "sun.max.fill")!
+        case .clouds:
+            return UIImage(systemName: "cloud.fill")!
+        case .drizzle:
+            return UIImage(systemName: "cloud.drizzle.fill")!
+        case .rain:
+            return UIImage(named: "rain")!
+        case .snow:
+            return UIImage(systemName: "cloud.snow.fill")!
+        case .thunderstorm:
+            return UIImage(systemName: "cloud.bolt.rain.fill")!
+        case .unknown:
+            return UIImage(systemName: "cloud")!
         }
     }
 }
