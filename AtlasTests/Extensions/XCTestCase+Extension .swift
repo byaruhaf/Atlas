3ab2821948449b4f4285403cccff382b0b0abbba
Franklin Byaruhanga <byaruhaf@appbantu.com>
2023-02-04 10:04:55 +0300

    Added Unit Test Day View Model

---
+++ AtlasTests/Extensions/XCTestCase+Extension .swift
@@ -0,0 +1,19 @@
+//
+//  XCTestCase+Extension .swift
+//  AtlasTests
+//
+//  Created by Franklin Byaruhanga on 04/02/2023.
+//
+
+import XCTest
+
+// swiftlint:disable force_try
+extension XCTestCase {
+    
+    func loadStub(name: String, extension: String) -> Data {
+        let bundle = Bundle(for: classForCoder)
+        let url = bundle.url(forResource: name, withExtension: `extension`)
+        
+        return try! Data(contentsOf: url!)
+    }
+}
---
+++ AtlasTests/Test Cases/DayViewModelTests.swift
@@ -0,0 +1,65 @@
+//
+//  DayViewModelTests.swift
+//  AtlasTests
+//
+//  Created by Franklin Byaruhanga on 04/02/2023.
+//
+
+import XCTest
+@testable import Atlas
+
+// swiftlint:disable force_try
+final class DayViewModelTests: XCTestCase {
+    
+    // MARK: - Properties
+    
+    var viewModel: DayViewModel!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        // Load Stub
+        let data = loadStub(name: "current", extension: "json")
+        
+        // Initialize JSON Decoder
+        let decoder = JSONDecoder()
+        
+        // Configure JSON Decoder
+        decoder.dateDecodingStrategy = .secondsSince1970
+        
+        // Initialize Dark Sky Response
+        let currentWeatherResponse = try! decoder.decode(CurrentWeather.self, from: data)
+        
+        // Initialize View Model
+        viewModel = DayViewModel(weatherData: currentWeatherResponse)
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+    
+    // MARK: - Tests for minTemperature
+    func testminTemperature() {
+        XCTAssertEqual(viewModel.minTemperature, "0.14 °")
+    }
+    
+    // MARK: - Tests for maxTemperature
+    func testmaxTemperature() {
+        XCTAssertEqual(viewModel.maxTemperature, "4.68 °")
+    }
+    
+    // MARK: - Tests for temperature
+    func testTemperature() {
+        XCTAssertEqual(viewModel.temperature, "-1.05 °")
+    }
+    
+    // MARK: - Tests for weatherCondition
+    func testWeatherCondition() {
+        XCTAssertEqual(viewModel.weatherCondition, "CLOUDY")
+    }
+    
+    // MARK: - Tests for backgroundImageName
+    func testBackgroundImageName() {
+        XCTAssertEqual(viewModel.backgroundImageName, "forest_cloudy")
+    }
+}
