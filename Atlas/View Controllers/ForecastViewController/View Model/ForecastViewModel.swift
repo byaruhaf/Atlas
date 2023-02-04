27cc2cbb62cdffe2870f708a125cdf333fa3105f
Franklin Byaruhanga <byaruhaf@appbantu.com>
2023-02-04 07:28:15 +0300

    Updated Time Display  for the forecast

--- Atlas/View Controllers/ForecastViewController/View Model/ForecastViewModel.swift
+++ Atlas/View Controllers/ForecastViewController/View Model/ForecastViewModel.swift
@@ -13,9 +13,15 @@ struct ForecastViewModel {
     var weeksDayWeatherData: [WeatherDayData] {
         var temp: [WeatherDayData] = []
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.dateFormat = "EE"
+        let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .short
         weatherData.list.forEach { ddd in
-            temp.append(WeatherDayData(day: "\(dateFormatter.string(from: ddd.date))", condition: "\(ddd.weather.first!.main)", temperature: ddd.main.feelsLike))
+            temp.append(
+                WeatherDayData(day: "  \(dateFormatter.string(from: ddd.date)) \(timeFormatter.string(from: ddd.date))",
+                               condition: "\(ddd.weather.first!.main)",
+                               temperature: ddd.main.feelsLike)
+            )
         }
         return temp
     }
