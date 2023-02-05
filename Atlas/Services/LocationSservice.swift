86f9bff116af78f580bdc4996e756623ce63126a
Franklin Byaruhanga <byaruhaf@appbantu.com>
2023-02-05 12:59:05 +0300

    added LocationSservice

---
+++ Atlas/Services/LocationSservice.swift
@@ -0,0 +1,83 @@
+//
+//  LocationSservice.swift
+//  Atlas
+//
+//  Created by Franklin Byaruhanga on 05/02/2023.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationSservice: NSObject, LocationServicing {
+    var locationContinuation: CheckedContinuation<Location, Error>?
+    var authorizationContinuation: CheckedContinuation<CLAuthorizationStatus?, Error>?
+    private let locationManager: CLLocationManager
+    lazy var isNotAuthorized: Bool = {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            return true
+        case .restricted:
+            return true
+        case .denied:
+            return true
+        case .authorizedAlways, .authorizedWhenInUse:
+            return false
+        @unknown default:
+            return true
+        }
+    }()
+    
+    override init() {
+        locationManager = CLLocationManager()
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    // MARK: - Helper Methods
+    func fetchUserLocation() async throws -> Location {
+        try await withCheckedThrowingContinuation { continuation in
+            self.locationContinuation = continuation
+            locationManager.requestLocation()
+        }
+    }
+    
+    func fetchUserAuthorizatio() async throws {
+        _ = try await withCheckedThrowingContinuation { continuation in
+            self.authorizationContinuation = continuation
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+}
+
+extension LocationSservice: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("Your location is restricted.") /// TODO: Log this with Logger
+            authorizationContinuation?.resume(throwing: WeatherDataError.notAuthorizedToRequestLocation)
+        case .denied:
+            print("You have denied this app location permission. Go to setting to change it") /// TODO: Log this with Logger
+            authorizationContinuation?.resume(throwing: WeatherDataError.notAuthorizedToRequestLocation)
+        case .authorizedAlways, .authorizedWhenInUse:
+            authorizationContinuation?.resume(returning: manager.authorizationStatus)
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latitude = locations.first?.coordinate.latitude else { return }
+        guard let longitude = locations.first?.coordinate.longitude else { return }
+        let location = Location(latitude: latitude, longitude: longitude)
+        locationContinuation?.resume(returning: location)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription) /// TODO: Log this with Logger
+        locationContinuation?.resume(throwing: WeatherDataError.failedToRequestLocation)
+    }
+}
