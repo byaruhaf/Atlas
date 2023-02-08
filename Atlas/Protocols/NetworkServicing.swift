ae2174ec485ee7151683a94f7d2761b74fa4cb27
Franklin Byaruhanga <byaruhaf@appbantu.com>
2023-02-08 18:18:36 +0300

    File improved order

--- Atlas/Protocols/NetworkServicing.swift
+++ Atlas/Protocols/NetworkServicing.swift
@@ -6,40 +6,10 @@
 //
 
 import Foundation
-import os.log
 
 protocol NetworkServicing {
     
     // MARK: - Methods
     func loadCurrentWeatherData(for location: Location) async throws -> CurrentWeather
     func loadForecastWeatherData(for location: Location) async throws -> Forecast
-}
-
-class NetworkSservice: NetworkServicing {
-    
-    func loadCurrentWeatherData(for location: Location) async throws -> CurrentWeather {
-        let weatherRequest = WeatherRequest(requestType: .weather, units: .metric, location: location)
-        return try await fetch(from: weatherRequest.urlRequest)
-    }
-    
-    func loadForecastWeatherData(for location: Location) async throws -> Forecast {
-        let weatherRequest = WeatherRequest(requestType: .forecast, units: .metric, location: location)
-        return try await fetch(from: weatherRequest.urlRequest)
-    }
-    
-    func fetch<T: Decodable>(from urlRequest: URLRequest) async throws -> T {
-        let (appNetData, response) = try await URLSession.shared.data(for: urlRequest)
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-            Logger.network.error("The response statusCode is \(String(describing: (response as? HTTPURLResponse)?.statusCode))")
-            throw  WeatherDataError.noWeatherDataAvailable
-        }
-        let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .secondsSince1970
-        do {
-            let result = try decoder.decode(T.self, from: appNetData)
-            return result
-        } catch {
-            throw  WeatherDataError.noWeatherDataAvailable
-        }
-    }
 }
---
+++ Atlas/Services/NetworkSservice.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkSservice.swift
+//  Atlas
+//
+//  Created by Franklin Byaruhanga on 08/02/2023.
+//
+
+import Foundation
+import os.log
+
+class NetworkSservice: NetworkServicing {
+    
+    func loadCurrentWeatherData(for location: Location) async throws -> CurrentWeather {
+        let weatherRequest = WeatherRequest(requestType: .weather, units: .metric, location: location)
+        return try await fetch(from: weatherRequest.urlRequest)
+    }
+    
+    func loadForecastWeatherData(for location: Location) async throws -> Forecast {
+        let weatherRequest = WeatherRequest(requestType: .forecast, units: .metric, location: location)
+        return try await fetch(from: weatherRequest.urlRequest)
+    }
+    
+    func fetch<T: Decodable>(from urlRequest: URLRequest) async throws -> T {
+        let (appNetData, response) = try await URLSession.shared.data(for: urlRequest)
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            Logger.network.error("The response statusCode is \(String(describing: (response as? HTTPURLResponse)?.statusCode))")
+            throw  WeatherDataError.noWeatherDataAvailable
+        }
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        do {
+            let result = try decoder.decode(T.self, from: appNetData)
+            return result
+        } catch {
+            throw  WeatherDataError.noWeatherDataAvailable
+        }
+    }
+}
